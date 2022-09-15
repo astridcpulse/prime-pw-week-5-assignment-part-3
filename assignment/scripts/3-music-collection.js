c48c98939b28abd103e3fabd5f19fd8856fd3df1
Astrid Pulse <astridcpulse@gmail.com>
2022-09-15 18:09:39 -0500

    completed addToCollection function and logged six albums into the collection array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,31 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection( title, artist, yearPublished){
+    let album = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+    };
+    collection.push(album);
+    return album;
+}
+
+addToCollection('Goths', 'the mountain goats', '2015');
+console.log(collection[0]); //album 1
+
+addToCollection('Bleed Out', 'the mountain goats', '2022');
+console.log(collection[1]); //album 2
+
+addToCollection('Whats Going On', 'Marvin Gaye', '1971');
+console.log(collection[2]); //album 3
+
+addToCollection('Hell Yeah', 'Hell Yeah', '2007');
+console.log(collection[3]); //album 4
+
+addToCollection('Big Idgies Big Day', 'The Big Idgies', '2022');
+console.log(collection[4]); //album 5
+
+addToCollection('The Truth About', 'GridLick', '2022');
+console.log(collection[5]); //album 6

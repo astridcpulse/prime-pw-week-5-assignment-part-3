273eae01e585288949086c8a22fc2ba792ac4110
Astrid Pulse <astridcpulse@gmail.com>
2022-09-15 18:14:56 -0500

    added a console log into the function to log each album as its added

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -9,23 +9,20 @@ function addToCollection( title, artist, yearPublished){
         yearPublished: yearPublished
     };
     collection.push(album);
+    console.log(album);
     return album;
 }
 
-addToCollection('Goths', 'the mountain goats', '2015');
-console.log(collection[0]); //album 1
+addToCollection('Goths', 'the mountain goats', '2015'); //album 1
 
-addToCollection('Bleed Out', 'the mountain goats', '2022');
-console.log(collection[1]); //album 2
+addToCollection('Bleed Out', 'the mountain goats', '2022'); //album 2
 
-addToCollection('Whats Going On', 'Marvin Gaye', '1971');
-console.log(collection[2]); //album 3
+addToCollection('Whats Going On', 'Marvin Gaye', '1971'); //album 3
 
-addToCollection('Hell Yeah', 'Hell Yeah', '2007');
-console.log(collection[3]); //album 4
+addToCollection('Hell Yeah', 'Hell Yeah', '2007'); //album 4
 
-addToCollection('Big Idgies Big Day', 'The Big Idgies', '2022');
-console.log(collection[4]); //album 5
+addToCollection('Big Idgies Big Day', 'The Big Idgies', '2022'); //album 5
 
-addToCollection('The Truth About', 'GridLick', '2022');
-console.log(collection[5]); //album 6
+addToCollection('The Truth About', 'GridLick', '2022');  //album 6
+
+console.log(collection);
